51b8d27f33c4b3b295237dcb3811d95eb2151e0a
Rwanda S <rwanda.ace@gmail.com>
2020-04-09 18:44:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,16 +5,15 @@ class Triangle
     @a = a 
     @b = b
     @c = c
-    if @a <= 0 || @b <= 0 || @c <= 0 || (@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-      puts error.message
-      end
-    end
   end
   def kind
-    if @a == @b && @a==@c
+    if @a <= 0 || @b <= 0 || @c <= 0 || (@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a
+     # begin
+        raise TriangleError
+      #rescue TriangleError => error
+      #puts error.message
+      #end
+    elsif @a == @b && @a==@c
       return :equilateral
     elsif (@a==@b && @a != @c) || (@b==@c && @b != @a) || (@a==@c && @a != @b)
     return :isosceles
