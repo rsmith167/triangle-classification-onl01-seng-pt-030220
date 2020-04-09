913055b78c96a08d2f896987f459737b5d6930ad
Rwanda S <rwanda.ace@gmail.com>
2020-04-09 17:50:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a,b,c)
+    @a = a 
+    @b = b
+    @c = c
+  end
+  def kind
+    if @a == @b && @a==c
+      return 
 end
