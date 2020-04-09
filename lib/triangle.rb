a1d05b60a1bf7b14e604081c9d94bbc125e4433f
Rwanda S <rwanda.ace@gmail.com>
2020-04-09 18:39:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,18 +7,19 @@ class Triangle
     @c = c
   end
   def kind
-    if @a == @b && @a==@c
-      return :equilateral
-    elsif (@a==@b && @a != @c) || (@b==@c && @b != @a) || (@a==@c && @a != @b)
-    return :isosceles
-  elsif (@a != @b) && (@b != @c) && (@a != @c)
-    return :scalene
-  elsif @a <= 0 || @b <= 0 || @c <= 0 || (@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a
+    if @a <= 0 || @b <= 0 || @c <= 0 || (@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a
   begin
   raise TriangleError
   rescue TriangleError => error
   puts error.message
 end
+elsif @a == @b && @a==@c
+      return :equilateral
+    elsif (@a==@b && @a != @c) || (@b==@c && @b != @a) || (@a==@c && @a != @b)
+    return :isosceles
+  elsif (@a != @b) && (@b != @c) && (@a != @c)
+    return :scalene
+  
   end
 end
 class TriangleError < StandardError
