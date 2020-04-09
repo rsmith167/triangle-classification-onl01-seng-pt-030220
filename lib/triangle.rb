22f3f540455f4e3d9f5747eef34a2cb523498e32
Rwanda S <rwanda.ace@gmail.com>
2020-04-09 18:35:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,12 @@ class Triangle
     return :isosceles
   elsif (@a != @b) && (@b != @c) && (@a != @c)
     return :scalene
-  elsif 
+  elsif (@a <= 0 || @b <= 0 || @c <= 0) || (@a + @b <= @c) || (@a + @c <= @b) || (@b + @c <= @a)
+  begin
+  raise TriangleError
+  rescue TriangleError => error
+  puts error.message
+end
   end
 end
 class TriangleError < StandardError
